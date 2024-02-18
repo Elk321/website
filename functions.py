0f5c01d231464b4e8222c3b91a2eb9a442c86921
ellen <chenellen50@gmail.com>
2024-02-18 22:47:24 +0100

    fix bug

--- functions.py
+++ functions.py
@@ -9,7 +9,7 @@ credentials = dict(st.secrets.google.cloud.storage.credentials)
 credentials = json.dumps(credentials)
 
 
-@st.cache_resource
+@st.cache_resource(ttl=600)
 def read_file(filename):
     conn = st.connection("gcs", type=FilesConnection)
     file = conn.read(f"bartabacchi_website/{filename}")
@@ -47,15 +47,13 @@ def update_debt_list(name, amount, username, lista_tabacchi):
         lista_tabacchi.at[row_number, "totale"] += amount
         total = lista_tabacchi.at[row_number, "totale"]
         if lista_tabacchi.at[row_number, "limite"]:
-            if (lista_tabacchi.at[row_number, "totale"] >
-                    lista_tabacchi.at[row_number, "limite"]):
+            if total > lista_tabacchi.at[row_number, "limite"]:
                 st.warning("Attenzione! Questo soggetto ha "
                            "superato il limite!")
     else:
         lista_tabacchi.loc[len(lista_tabacchi)] = [name, amount, " "]
-
-    upload_file("lista/lista_debiti_tabacchi.csv",
-                lista_tabacchi)
+        upload_file("lista/lista_debiti_tabacchi.csv",
+                    lista_tabacchi)
 
     return total, lista_tabacchi
 
@@ -244,7 +242,6 @@ def current_page_tabacchi(name_list, object_list, lista_tabacchi, blacklist, deb
                 button1 = st.form_submit_button("Aggiungi")
                 if button1:
                     name_list = add_name(new_name, name_list)
-                    st.rerun()
 
     with st.form(key="add_debt", clear_on_submit=True, border=False):
         row2 = st.columns(4)
@@ -283,7 +280,6 @@ def current_page_tabacchi(name_list, object_list, lista_tabacchi, blacklist, deb
                                                  hour=time,
                                                  filename=f"date/tabacchi/{current_date}.csv")
                     st.success("Aggiunto!")
-                    st.rerun()
 
     show_debt(f"date/tabacchi/{date}.csv",
               f"date/tabacchi/{current_date}.csv",
